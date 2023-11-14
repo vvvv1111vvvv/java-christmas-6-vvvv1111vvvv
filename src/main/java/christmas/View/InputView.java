f09a299aeb13ec2f9dd210a20b4434f723517c37
vvvv1111vvvv <105331246+vvvv1111vvvv@users.noreply.github.com>
2023-11-14 18:46:36 +0900

    refactor: 기능에 따라 클래스 분리. InputView, OutputView

---
+++ src/main/java/christmas/View/InputView.java
@@ -0,0 +1,40 @@
+package christmas.View;
+
+import camp.nextstep.edu.missionutils.Console;
+import christmas.Model.ValidationChecker;
+
+import java.text.DecimalFormat;
+import java.util.List;
+
+
+
+
+public class InputView {
+    public void inputView(){
+        System.out.println(TextMsg.WELCOME.label);
+    }
+    public int readDate() {
+        int temp;
+        System.out.println(TextMsg.ASK_DATE.label);
+        do {
+            String input = Console.readLine();
+            temp = ValidationChecker.dateChecker(input);
+        }
+        while (temp == 0);
+        return temp;
+    }
+
+    public List<List<String>> readMenu() {
+        List<List<String>> temp_str;
+        System.out.println(TextMsg.ASK_DISHES.label);
+        do {
+            String input = Console.readLine();
+            temp_str = ValidationChecker.menuChecker(input);
+
+        }
+        while (temp_str == null);
+        return temp_str;
+    }
+}
+
+
