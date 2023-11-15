62215946983d73daf0c6dd19cc91dcb654fdafd1
vvvv1111vvvv <105331246+vvvv1111vvvv@users.noreply.github.com>
2023-11-15 09:02:10 +0900

    fix: enum 출력시 label이 출력되도록 수정

--- src/main/java/christmas/View/OutputView.java
+++ src/main/java/christmas/View/OutputView.java
@@ -8,62 +8,74 @@ public class OutputView{
 
     public void printDate(int month, int day) {
         System.out.println(month + TextMsg.VAR_MONTH.label + " "
-                + day + TextMsg.SHOW_INTRO);
+                + day + TextMsg.SHOW_INTRO.label);
     }
 
     public void printOrder(List<List<String>> menu) {
-        System.out.println(TextMsg.SHOW_ORDERED_MENU);
+        System.out.println();
+        System.out.println(TextMsg.SHOW_ORDERED_MENU.label);
         for (int i = 0; i < menu.size(); i++) {
             System.out.println(menu.get(i).get(0) + " " + menu.get(i).get(1)
-                    + TextMsg.VAR_COUNT);
+                    + TextMsg.VAR_COUNT.label);
         }
+        System.out.println();
     }
 
     public void printBeforeTotal(int price) {
-        System.out.println(TextMsg.SHOW_INIT_TOTAL_PRICE);
-        System.out.println(decFormat.format(price) + TextMsg.VAR_WON);
+        System.out.println(TextMsg.SHOW_INIT_TOTAL_PRICE.label);
+        System.out.println(decFormat.format(price) + TextMsg.VAR_WON.label);
+        System.out.println();
     }
 
-    public void printGiftMenu(String gift, int giftCount) {
-        System.out.println(TextMsg.SHOW_GIFT_MENU);
-        if (giftCount == 0) {
-            System.out.println(TextMsg.NO);
+    public void printGiftMenu(String giftProduct, int giftCount, boolean gift) {
+        System.out.println(TextMsg.SHOW_GIFT_MENU.label);
+        if (gift == false) {
+            System.out.println(TextMsg.NO.label);
         }
-        if (giftCount == 1) {
-            System.out.println(gift + " " + giftCount + TextMsg.VAR_COUNT);
+        if (gift == true) {
+            System.out.println(giftProduct + " " + giftCount + TextMsg.VAR_COUNT.label);
         }
+        System.out.println();
     }
 
     public void printBenefitIndex(List<List<String>> benefits) {
-        System.out.println(TextMsg.SHOW_BENEFITS);
+        System.out.println(TextMsg.SHOW_BENEFITS.label);
         if (benefits == null) {
-            System.out.println(TextMsg.NO);
+            System.out.println(TextMsg.NO.label);
         }
         if (benefits != null) {
             for (int i = 0; i < benefits.size(); i++) {
-                System.out.println(benefits.get(i).get(0) + TextMsg.COL + " -"
-                        + decFormat.format(benefits.get(i).get(1)) + TextMsg.VAR_WON);
+                System.out.println(benefits.get(i).get(0) + TextMsg.COL.label + " -"
+                        + decFormat.format(Integer.parseInt(benefits.get(i).get(1))) + TextMsg.VAR_WON.label);
             }
         }
+        System.out.println();
     }
 
     public void printTotalBenefits(int price) {
-        System.out.println(TextMsg.SHOW_TOTAL_BENEFITS);
-        System.out.print("-" + decFormat.format(price) + TextMsg.VAR_WON);
+        System.out.println(TextMsg.SHOW_TOTAL_BENEFITS.label);
+        String minus="-";
+        if(price==0){
+            minus="";
+        }
+        System.out.println(minus + decFormat.format(price) + TextMsg.VAR_WON.label);
+        System.out.println();
     }
 
     public void printTotalPrice(int price) {
-        System.out.println(TextMsg.SHOW_EXPECTED_TOTAL_PRICE);
-        System.out.println(decFormat.format(price) + TextMsg.VAR_WON);
+        System.out.println(TextMsg.SHOW_EXPECTED_TOTAL_PRICE.label);
+        System.out.println(decFormat.format(price) + TextMsg.VAR_WON.label);
+        System.out.println();
     }
 
     public void printEventBadge(String badge){
-        System.out.println(TextMsg.EVENT_BEDGE);
+        System.out.println(TextMsg.EVENT_BEDGE.label);
         if(badge==null){
-            System.out.println(TextMsg.NO);
+            System.out.println(TextMsg.NO.label);
         }
         if(badge!=null){
             System.out.println(badge);
         }
+        System.out.println();
     }
 }
