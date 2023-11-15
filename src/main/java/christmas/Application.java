8cd27564f6e6a326cb210844985236a77ac9e8be
vvvv1111vvvv <105331246+vvvv1111vvvv@users.noreply.github.com>
2023-11-15 09:00:24 +0900

    feat: application 전체 동작 수행 기능 추가

--- src/main/java/christmas/Application.java
+++ src/main/java/christmas/Application.java
@@ -1,7 +1,12 @@
 package christmas;
 
+import christmas.Controller.Controller;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        Controller control = new Controller();
+        control.getDateandOrder();
+        control.calculation();
+        control.printResult();
     }
 }
